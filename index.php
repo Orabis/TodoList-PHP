8ebdc26fe311ebc3312196e740abf3fb3fcebaa3
lmerkel <lmerkel@unistra.fr>
2026-05-26 14:33:47 +0200

    set up connexion

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASSWORD");
+$db = getenv("DB_NAME");
+$port = getenv("DB_PORT");
+$host = getenv("DB_HOST");
+
+try {
+    $pdo = new PDO("pgsql:host={$host};port={$port};dbname={$db}", $user, $pass);
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+$sql = "SELECT * FROM priorites";
+foreach ($pdo->query($sql) as $row) {
+    print $row['nom_priorite'];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,6 +27,15 @@
 </head>
 <body>
 <main>
+    <hgroup>
+        <h1>To Do List </h1>
+        <p>Liste des tâches à faire</p>
+    </hgroup>
+    <div>
+        <form method="POST" action="">
+            <h2>Crée une nouvelle tâche</h2>
+        </form>
+    </div>
 </main>
 <footer>
     Léo Merkel
